66be54c00ec43e1795c11877e00b2350572f7811
mrowlingfox <mitchell.rowling@foxsports.com.au>
2020-06-14 00:06:31 +1000

    Remove if statement that I think won't work

--- exposestrategy/ingress.go
+++ exposestrategy/ingress.go
@@ -150,9 +150,7 @@ func (s *IngressStrategy) Add(svc *api.Service) error {
 		ingress.Annotations["fabric8.io/generated-by"] = "exposecontroller"
 	}
 
-	if secondHostName != nil {
-		ingress.Annotations["nginx.ingress.kubernetes.io/server-alias"] = fmt.Sprintf("%s, %s", hostName, aliasHostName)
-	}
+	ingress.Annotations["nginx.ingress.kubernetes.io/server-alias"] = fmt.Sprintf("%s, %s", hostName, aliasHostName)
 
 	hasOwner := false
 	for _, o := range ingress.OwnerReferences {
