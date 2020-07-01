f3e1c4d5516ab2180b2a21554b498ff1f3bd18e3
Nathan Wong <nathan.wong@foxsports.com.au>
2020-07-01 15:59:42 +1000

    remove debug

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,9 +20,6 @@ pipeline {
           sh "echo \$(jx-release-version) > VERSION"
           sh "jx step tag --version \$(cat VERSION)"
 
-          // Debug
-          sh "whoami"
-
           // Build binary
           sh "git clone git://github.com/jenkins-x/exposecontroller.git \$GOPATH/src/github.com/jenkins-x/exposecontroller"
           sh "cd \$GOPATH/src/github.com/jenkins-x/exposecontroller"
