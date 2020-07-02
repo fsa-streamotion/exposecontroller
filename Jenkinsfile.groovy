6c9f84ebd8e2f19adca8141cc1148c944d07c3a2
Nathan Wong <59549970+nathanwong91@users.noreply.github.com>
2020-07-03 09:01:28 +1000

    Create Pipeline to Build and Push to ECR (#4)
    * put make on same line
    
    * copy binary
    
    * bad copy pasta
    
    * add jenkins step for PR
    
    * add jenkins step for master
    
    * incorrect PR_VERSION
    
    * Update Dockerfile
    
    * use streamotion repo
    
    * remove go vet and change labels

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,86 @@
+pipeline {
+  agent {
+      label "jenkins-go"
+  }
+
+  environment {
+    ORG = 'fsa-streamotion'
+    APP_NAME = 'exposecontroller'
+  }
+
+  stages {
+    stage('Build PR') {
+      when {
+          branch 'PR-*'
+      }
+
+      environment {
+          PR_VERSION = "$BRANCH_NAME-$BUILD_NUMBER"
+      }
+
+      steps {
+        container('go') {
+            sh "git config --global credential.helper store"
+            sh "jx step git credentials"
+
+            // Make Test
+            sh "git clone git://github.com/fsa-streamotion/exposecontroller.git \$GOPATH/src/github.com/jenkins-x/exposecontroller"
+            sh "cd \$GOPATH/src/github.com/jenkins-x/exposecontroller && make test"
+
+            // Copy binary
+            sh "mkdir out"
+            sh "cp \$GOPATH/src/github.com/jenkins-x/exposecontroller/out/exposecontroller-linux-amd64 ./out"
+
+            // Build Image and push to ECR
+            sh "export VERSION=\$PR_VERSION && skaffold build -f skaffold.yaml"
+
+            script {
+                currentBuild.displayName = PR_VERSION
+                currentBuild.description = "${DOCKER_REGISTRY}/$ORG/$APP_NAME:$PR_VERSION"
+            }
+          }
+        }
+      }
+        
+    stage('Build Master') {
+      when {
+            branch 'master'
+          }
+      steps {
+        container('go') {
+
+          sh "git config --global credential.helper store"
+          sh "jx step git credentials"
+
+          sh "echo \$(jx-release-version) > VERSION"
+          sh "jx step tag --version \$(cat VERSION)"
+
+          // Build binary
+          sh "git clone git://github.com/fsa-streamotion/exposecontroller.git \$GOPATH/src/github.com/jenkins-x/exposecontroller"
+          sh "cd \$GOPATH/src/github.com/jenkins-x/exposecontroller && make"
+          
+          // Copy binary
+          sh "mkdir out"
+          sh "cp \$GOPATH/src/github.com/jenkins-x/exposecontroller/out/exposecontroller-linux-amd64 ./out"
+
+          // Build image and push to ECR
+          sh "skaffold version"
+          sh "export VERSION=`cat VERSION` && skaffold build -f skaffold.yaml"
+          sh "export VERSION=latest && skaffold build -f skaffold.yaml"
+
+          script {
+            def buildVersion =  readFile "${env.WORKSPACE}/VERSION"
+            currentBuild.description = "${DOCKER_REGISTRY}/exposecontroller:$buildVersion"
+            currentBuild.displayName = "$buildVersion"
+          }
+        }
+      }
+    }
+  }
+
+  post {
+    always {
+      cleanWs()
+    }
+  }
+}
