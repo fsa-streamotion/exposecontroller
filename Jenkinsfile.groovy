7253008ef1b6c8b4aa6967d26a2c450dbd9e5a05
Nathan Wong <nathan.wong@foxsports.com.au>
2020-07-02 10:10:29 +1000

    add debugging for path

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,22 +19,33 @@ pipeline {
 
           sh "echo \$(jx-release-version) > VERSION"
           sh "jx step tag --version \$(cat VERSION)"
-          
+
           // Install Go
+          sh "pwd" // Debug for Jenkins weirdness
           sh "curl -O https://storage.googleapis.com/golang/go1.14.4.linux-amd64.tar.gz"
           sh "tar -xvf go1.14.4.linux-amd64.tar.gz"
           sh "chown -R root:root ./go"
+          sh "pwd" // Debug for Jenkins weirdness
           sh "mv go /usr/local"
+          sh "pwd" // Debug for Jenkins weirdness
           sh "export GOPATH=\$HOME/go"
+          sh "pwd" // Debug for Jenkins weirdness
           sh "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin"
+          sh "pwd" // Debug for Jenkins weirdness
 
           // Build binary
-          sh "git clone git://github.com/jenkins-x/exposecontroller.git \$GOPATH/src/github.com/jenkins-x/exposecontroller"
-          sh "cd \$GOPATH/src/github.com/jenkins-x/exposecontroller && make"
+          sh "pwd" // Debug for Jenkins weirdness
+          sh "git clone git://github.com/jenkins-x/exposecontroller.git \$HOME/go/src/github.com/jenkins-x/exposecontroller"
+          sh "pwd" // Debug for Jenkins weirdness
+          sh "cd \$HOME/go/src/github.com/jenkins-x/exposecontroller && make"
+          sh "pwd" // Debug for Jenkins weirdness
 
           // Copy binary
+          sh "pwd" // Debug for Jenkins weirdness
           sh "mkdir out"
+          sh "pwd" // Debug for Jenkins weirdness
           sh "cp \$GOPATH/src/github.com/jenkins-x/exposecontroller/out/exposecontroller-linux-amd64 ./out"
+          sh "pwd" // Debug for Jenkins weirdness
 
           // Build image
           sh "skaffold version"
